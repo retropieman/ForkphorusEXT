18a1435e617ae8e59422f3319af526542e147d62
Witherman17 <31626399+retropieman@users.noreply.github.com>
2019-11-09 09:27:32 -0700

    Update ForkphorusEXT.js

--- ForkphorusEXT.js
+++ ForkphorusEXT.js
@@ -18,12 +18,17 @@
     ext.gotox = function(xid, callback) {
         window.open('https://forkphorus.github.io/#' + xid,'1573258053476','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;
     };
+    ext.goproj = function(xid, callback) {
+        
+        window.open('https://forkphorus.github.io/app.html?id=' + xid,'1573258053476','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;
+    }
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['w', 'Open google popup', 'ogoogs'],
             ['w', 'Open project with ID %n', 'gotox'],
+            ['w', 'Open packaged project with ID %n, 'goproj'],
         ]
     };
 
