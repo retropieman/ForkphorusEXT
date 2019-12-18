454d51d5e9e59731ea69326213e174efaaa07530
Witherman17 <31626399+retropieman@users.noreply.github.com>
2019-12-18 16:01:38 -0700

    Update ForkphorusEXT.js

--- ForkphorusEXT.js
+++ ForkphorusEXT.js
@@ -22,7 +22,17 @@
     ext.goproj = function(xid, callback) {
         
         window.open('https://forkphorus.github.io/app.html?id=' + xid,'1573258053476','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');return false;
-    }
+    };
+    ext.dload = function(filename,text) {
+        // http://stackoverflow.com/questions/3665115/create-a-file-in-memory-for-user-to-download-not-through-server
+        var sacrifice=document.createElement('a');
+        sacrifice.setAttribute('href','data:text/plain;charset=utf-8,'+encodeURIComponent(text));
+        sacrifice.setAttribute('download',filename);
+        sacrifice.style.display='none';
+        document.body.appendChild(sacrifice);
+        sacrifice.click();
+        document.body.removeChild(sacrifice);
+     };
 
     // Block and block menu descriptions
     var descriptor = {
@@ -30,6 +40,7 @@
             ['w', 'Open google popup', 'ogoogs'],
             ['w', 'Open project with ID %n', 'gotox'],
             ['w', 'Open packaged project with ID %n', 'goproj'],
+            ['w', 'Donload file with name %s', 'dload'],
         ]
         url: "https://retropieman.github.io/ForkphorusEXT/",
         displayName: "Pop-ups"
